84d8cebaae94810a1723767d3d42686f49c4daea
Ak1ro666 <toxafedorov4@gmail.com>
2024-03-29 20:19:29 +1000

    fix bugs

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import App from './app/App';
 import { MainProvider } from '@/app/providers/MainProvider'
 import { createRoot } from 'react-dom/client';
-import { someFn } from './test';
-
-someFn(17);
 
 const container = document.getElementById('root');
 const root = createRoot(container);
